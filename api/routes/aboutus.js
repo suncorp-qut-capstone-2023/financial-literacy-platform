fb707237e55a59746870fc6c239f7a34d4090f12
Meet Vadher <58776875+meet-vadher@users.noreply.github.com>
2023-06-12 23:54:04 +1000

    Added unit testing
    Added unit tests for each API endpoints using Jest framework

--- api/routes/aboutus.js
+++ api/routes/aboutus.js
@@ -4,41 +4,33 @@ var router = express.Router();
 
 // A static route for use of example.
 router.get("/about", function (req, res, next) {
-    res
-        .json({
-            name: "JCMG",
-            description: "A group of four students from QUT, working on their final year capstone project. The project is to create an API server for Suncorp-Metway.",
-            company: "Suncorp-Metway",
-            members: [
-                {
-                    name: "Jason Finsalyne",
-                    student_id: "n10592024",
-                    email: "n10592024@qut.edu.au"
-                },
-                {
-                    name: "Christopher Mai",
-                    student_id: "n9712364",
-                    email: "n9712364@qut.edu.au"
-                },
-                {
-                    name: "Meet Vadher",
-                    student_id: "n11012595",
-                    email: "n11012595@qut.edu.au"
-                },
-                {
-                    name: "Geoffrey Beckett",
-                    student_id: "n11066148",
-                    email: "geoffrey.beckett@connect.qut.edu.au"
-                }
-            ],
-        })
-        .catch((err) => {
-            console.log(err);
-            res.json({
-                error: true,
-                message: "Error in getting data",
-            });
-        });
+    res.json({
+        name: "JCMG",
+        description: "A group of four students from QUT, working on their final year capstone project. The project is to create an API server for Suncorp-Metway.",
+        company: "Suncorp-Metway",
+        members: [
+            {
+                name: "Jason Finsalyne",
+                student_id: "n10592024",
+                email: "n10592024@qut.edu.au"
+            },
+            {
+                name: "Christopher Mai",
+                student_id: "n9712364",
+                email: "n9712364@qut.edu.au"
+            },
+            {
+                name: "Meet Vadher",
+                student_id: "n11012595",
+                email: "n11012595@qut.edu.au"
+            },
+            {
+                name: "Geoffrey Beckett",
+                student_id: "n11066148",
+                email: "geoffrey.beckett@connect.qut.edu.au"
+            }
+        ],
+    });
 });
 
 module.exports = router;
