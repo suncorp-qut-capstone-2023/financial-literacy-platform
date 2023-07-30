ae91bd496e03fd46445e070e06c6e453ca9dfde1
Geoffrey Beckett <Geoffrey.beckett@connect.qut.edu.au>
2023-07-30 17:08:14 +1000

    Grammar fixes and remove /api/learningmodules/{moduleID}/viewContent

--- api/__tests__/api.test.js
+++ api/__tests__/api.test.js
@@ -68,12 +68,16 @@ describe('GET /api/learningmodules', () => {
     });
 
     it('should retrieve single course information', async () => {
-        const courseID = randomCourseId()
-        const response = await request(app).get('/api/learningmodules?course='+courseID);
-
+        const response = await request(app).get(`/api/learningmodules/${courseID}`);
+    
         expect(response.statusCode).toBe(200);
-        expect(response.body).toHaveProperty('course.course_id', courseID);
+    
+        const foundCourse = response.body['available courses'].find(course => course.course_id === courseID);
+    
+        expect(foundCourse).not.toBeNull();
+        expect(foundCourse).toHaveProperty('course_id', courseID);
     });
+    
 });
 
 describe('GET /api/learningmodules/media', () => {
@@ -81,42 +85,55 @@ describe('GET /api/learningmodules/media', () => {
     const videoID = randomMediaVideoID()
 
     it('should retrieve a media image for a course', async () => {
-        const response = await request(app).get('/api/learningmodules/media?image='+imageID);
-
+        const imageID = 1; // Make sure this ID exists in your database
+        const response = await request(app).get(`/api/learningmodules/media?image=${imageID}`);
+    
         expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('image', `http://127.0.0.1:443/api/learningModules/${imageID}/image`);
     });
+    
 
     it('should retrieve a media video for a course', async () => {
-        const response = await request(app).get('/api/learningmodules/media?video='+videoID);
-
+        const videoID = 1; // Make sure this ID exists in your database
+        const response = await request(app).get(`/api/learningmodules/media?video=${videoID}`);
+    
         expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('video', `http://127.0.0.1:443/api/learningModules/${videoID}/video`);
     });
+    
 
     it('should give an error, when both image and video id is passed', async () => {
-        const courseID = randomCourseId()
-        const response = await request(app).get('/api/learningmodules/media?image='+imageID+'&video='+videoID);
-
+        const imageID = 1; // Make sure this ID exists in your database
+        const videoID = 2; // Make sure this ID exists in your database
+        const response = await request(app).get(`/api/learningmodules/media?image=${imageID}&video=${videoID}`);
+    
         expect(response.statusCode).toBe(400);
         expect(response.body).toHaveProperty('error', true);
         expect(response.body).toHaveProperty('message', 'Bad request, please specify an image ID OR video ID');
     });
+    
 });
 
-describe('GET /api/learningmodules/{moduleID}/viewContent', () => {
-    const moduleID = randomModuleID()
+// TODO(Geoffrey): This endpoint does not exist anymore. Please update
+// describe('GET /api/learningmodules/{moduleID}/viewContent', () => {
+//     const moduleID = randomModuleID()
 
-    it('should retrieve the contents of a module', async () => {
-        const response = await request(app).get(`/api/learningmodules/${moduleID}/viewContent`);
+//     it('should retrieve the contents of a module', async () => {
+//         const moduleID = 1; // Ensure this module ID exists in your database
+//         const response = await request(app).get(`/api/learningmodules/${moduleID}/viewContent`);
+    
+//         expect(response.statusCode).toBe(200);
+//         expect(response.body).toHaveProperty('module_information');
+//     });
+    
 
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toHaveProperty('module_information')
-    });
-
-    it('should give error, for a unrecognised module', async () => {
-        const response = await request(app).get(`/api/learningmodules/A1B2C3/viewContent`);
-
-        expect(response.statusCode).toBe(404);
-        expect(response.body).toHaveProperty('error', true)
-        expect(response.body).toHaveProperty('message', 'Module not found')
-    });
+//     it('should give error, for an unrecognised module', async () => {
+//         const moduleID = 999; // Use a non-existing module ID for this test
+//         const response = await request(app).get(`/api/learningmodules/${moduleID}/viewContent`);
+    
+//         expect(response.statusCode).toBe(404);
+//         expect(response.body).toHaveProperty('error', true);
+//         expect(response.body).toHaveProperty('message', 'Module not found');
+//     });
+    
 });
