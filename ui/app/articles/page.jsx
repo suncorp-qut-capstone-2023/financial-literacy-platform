760a50e151e05a5882fdc85d7d787c7d99c1de4f
Anhad Jangra <jangraanhad@gmail.com>
2023-08-04 16:46:13 +1000

    Changed Files to JSX and Created a Basic Template

---
+++ ui/app/articles/page.jsx
@@ -0,0 +1,44 @@
+import styles from "../../styles/page.module.css";
+
+export default function Article() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>THIS IS A PLACEHOLDER FOR THE ARTICLE CONTENT</h1>
+      </div>
+
+      <div className={styles.description}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
+          similique necessitatibus saepe aperiam, ullam placeat nisi cupiditate
+          ipsa laboriosam rerum aut quod quibusdam dicta, consectetur at veniam
+          velit voluptatibus voluptatem.
+        </p>
+        <p>
+          Maxime excepturi deserunt accusantium est, temporibus similique sunt
+          rerum vero pariatur rem porro repudiandae ad libero! Quibusdam
+          asperiores modi corporis nisi, eligendi numquam, quam exercitationem
+          error commodi quae, amet obcaecati?
+        </p>
+        <p>
+          Voluptas pariatur laboriosam, fugit eum eius voluptatem rerum! Eaque
+          optio excepturi, consequuntur commodi tempora sequi veniam nihil a
+          quibusdam sit ipsum pariatur quidem voluptatibus distinctio, quis,
+          numquam possimus autem consectetur?
+        </p>
+        <p>
+          Soluta nisi doloremque aperiam facilis. Sapiente adipisci sint harum
+          amet eaque nemo quidem facilis inventore consectetur, aspernatur
+          debitis officia explicabo placeat? Consectetur animi enim perferendis
+          molestias officiis aliquid veritatis amet.
+        </p>
+        <p>
+          Nobis impedit id dolorum nostrum omnis natus quia. Cum, corporis quod
+          dolor iure iusto eveniet error velit neque ipsam eos voluptatibus
+          laudantium repudiandae explicabo, rerum nostrum dignissimos assumenda
+          aut nisi.
+        </p>
+      </div>
+    </main>
+  );
+}
---
+++ ui/components/footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <h2>This will be the footer</h2>
+    </footer>
+  );
+}
