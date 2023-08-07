aa10005f8aae37f226462bf4b90c51b4c1ea9e74
Kriss Andrew <Kriss1Andrew@gmail.com>
2023-08-07 12:34:30 +1000

    added mui navbar

--- ui/app/layout.jsx
+++ ui/app/layout.jsx
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-// import Navbar from "../components/navbar"
+import AppBar from "@/components/appbar";
 import Footer from "../components/footer";
 import { Inter } from "next/font/google";
 
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Navbar /> */}
+        <AppBar />
         {children}
         <Footer />
       </body>
