d33d293917281016c74929642ddef27b62092717
Anhad Jangra <jangraanhad@gmail.com>
2023-08-10 22:39:12 +1000

    Create templates for video materials pages

---
+++ ui/app/videos/page.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import styles from "@/styles/page.module.css";
+import Video from "@/components/video";
+
+
+export default function Articles(/* TODO: pass in some api call */) {
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1 className={styles.title}>Video Materials</h1>
+      </div>
+      
+      <Grid container spacing={2}>
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </Grid>
+    </main>
+  );
+}
