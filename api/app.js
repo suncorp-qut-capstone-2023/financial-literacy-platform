f23234a3adb519592b8ce46b7c3d90da8cc7cd96
Geoffrey Beckett <Geoffrey.beckett@connect.qut.edu.au>
2023-08-12 01:41:55 +1000

    Removed DB stuff until meet branch is back

--- api/app.js
+++ api/app.js
@@ -12,8 +12,9 @@ const cors = require("cors");
 require("dotenv").config();
 
 // database setup
-const options = require('./knexfile');
-const knex = require("knex")(options);
+// TODO(anyone): When db is back reinstate below lines
+// const options = require('./knexfile');
+// const knex = require("knex")(options);
 
 // swagger setup
 const swaggerUI = require("swagger-ui-express");
@@ -29,10 +30,11 @@ const learningModulesRouter = require('./routes/learningModules.js');
 const enrolmentRouter = require('./routes/enrolment.js'); 
 
 // database connection
-app.use((req,res,next) => {
-    req.db = knex;
-    next();
-})
+// TODO(anyone): When db is back reinstate below lines
+// app.use((req,res,next) => {
+//     req.db = knex;
+//     next();
+// })
 
 // security implementation
 app.use(helmet());
@@ -62,12 +64,13 @@ logger.token('res', (req, res) => {
 });
 
 // check if knex is working
-app.get('/api/knex', function(req,res,next){
-    req.db.raw("SELECT VERSION()")
-        .then((version) => console.log(version[0][0]))
-        .catch((err) => {console.log(err); throw err;})
-    res.send("Version logged successfully")
-});
+// TODO(anyone):Please reinstate when back to normal
+// app.get('/api/knex', function(req,res,next){
+//     req.db.raw("SELECT VERSION()")
+//         .then((version) => console.log(version[0][0]))
+//         .catch((err) => {console.log(err); throw err;})
+//     res.send("Version logged successfully")
+// });
 
 // swagger implementation
 app.use('/api/docs', swaggerUI.serve);
