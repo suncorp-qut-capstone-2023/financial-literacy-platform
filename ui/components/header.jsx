1c07512a5ac0042fbd30bf0dcecee75090e1d9ec
Anhad Jangra <jangraanhad@gmail.com>
2023-08-16 13:40:45 +1000

    Header image changes based on screen width

--- ui/components/header.jsx
+++ ui/components/header.jsx
@@ -1,22 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 // Header component.
 import Image from "next/image";
 import Typography from "@mui/material/Typography";
 
 import styles from "@/styles/header.module.css";
-import businessWoman from '../assets/businesswoman.png'
+
+import businessWoman from "../assets/businesswoman.png";
+import businessWomanWide from "../assets/businesswomanwide.png";
 
 const { heroContent, heroWrapper, imageWrapper } = styles;
 
 export default function Header() {
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    // Function to update window width
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Initial width
+    handleResize();
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const imageSrc = windowWidth > 900 ? businessWomanWide : businessWoman;
+
   return (
     <div className={heroWrapper}>
       <div className={imageWrapper}>
         <Image
           priority={true}
-          src={businessWoman}
+          src={imageSrc}
           layout="fill"
           objectFit="cover"
-          objectPosition="50% 25%"  // Adjust these values as needed
+          objectPosition="50% 25%" // Adjust these values as needed
           alt="hero image example"
         />
       </div>
