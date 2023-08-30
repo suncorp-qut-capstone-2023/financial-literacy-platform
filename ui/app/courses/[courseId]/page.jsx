f98a755f618bfdb516eb13d48d24ae23024a551d
Anhad Jangra <jangraanhad@gmail.com>
2023-08-30 19:07:38 +1000

    Restructured courses directory to enable dynamic routing

--- ui/app/courses/[courseId]/page.jsx
+++ ui/app/courses/[courseId]/page.jsx
@@ -1,12 +1,12 @@
 "use client";
 
-export default function CoursePage() {
+export default function CoursePage({params}) {
 
   console.log("Course Page is being accessed!");
 
   return (
     <div>
-      This is course page with ID: {courseId}
+      This is course page with ID: {params.courseId}
     </div>
   );
 }
