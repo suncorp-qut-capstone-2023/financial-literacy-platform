7f987010dc82d82a21fb2c698fc008e2a0a1c03d
Geoffrey Beckett <Geoffrey.beckett@connect.qut.edu.au>
2023-09-28 19:26:12 +1000

    logic should be close to finished

--- api/models/enrolment.js
+++ api/models/enrolment.js
@@ -32,7 +32,52 @@ class Enrolment {
     return knex("quizzes_attempted").insert(attemptData);
   }
 
+  static async findRegistration(userId, courseId) {
+    return knex('course_registration')
+      .where({
+        user_id: userId,
+        course_id: courseId
+      })
+      .first();
+  }
+
+  static async countTotalQuizzes(courseId) {
+    return knex('quiz')
+      .join('module', 'quiz.MODULE_ID', 'module.MODULE_ID')
+      .where('module.COURSE_ID', courseId)
+      .countDistinct('quiz.QUIZ_ID as total');
+  }
+
+  static async countAttemptedQuizzes(registrationId) {
+    return knex('quizzes_attempted')
+      .where('registration_id', registrationId)
+      .countDistinct('quiz_id as total');
+  }
+
+  static async markCourseCompleted(registrationId) {
+    return knex('course_registration')
+      .where('registration_id', registrationId)
+      .update({
+        completed: true,
+        completion_date: new Date().toISOString()
+      });
+  }
+
+  static async markCourseCompleted(registrationId) {
+    const currentDate = new Date();
+    const expirationDate = new Date(currentDate);
+    expirationDate.setFullYear(expirationDate.getFullYear() + 1);  // Set the date to one year in the future
   
+    return knex('course_registration')
+      .where('registration_id', registrationId)
+      .update({
+        completed: true,
+        completion_date: currentDate.toISOString(),
+        expiration_date: expirationDate.toISOString()
+      });
+  }
+
+
 }
 
 module.exports = Enrolment;
