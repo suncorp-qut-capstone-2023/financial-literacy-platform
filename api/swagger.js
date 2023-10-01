2bc971c36fd405033422f53df413409a72c4da30
Geoffrey Beckett <Geoffrey.beckett@connect.qut.edu.au>
2023-10-02 01:13:32 +1000

    FIx swagger on Heroku
    Found missing dependencies due to outdated packages

--- api/swagger.js
+++
@@ -1,6 +0,0 @@
-const swaggerAutogen = require('swagger-autogen')()
-
-const outputFile = './swaggerOutput.json'
-const endpointsFiles = ['./routes/learningModules.js']
-
-swaggerAutogen(outputFile, endpointsFiles)
