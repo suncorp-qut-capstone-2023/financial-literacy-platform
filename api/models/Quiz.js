fa62be090800457302397101a735905be2b97311
Geoffrey Beckett <Geoffrey.beckett@connect.qut.edu.au>
2023-10-04 15:10:43 +1000

    Quiz done, Start questions

--- api/models/Quiz.js
+++ api/models/Quiz.js
@@ -1,26 +1,43 @@
 const knexOptions = require('../db/mydb-connection.js');
 const knex = require("knex")(knexOptions);
 
-class Quiz{
-
+class Quiz {
+    
     static getAllQuizzes(courseID, moduleID) {
-        return knex('modules').select("*").where('course_id', '=', courseID).andWhere('module_id', '=', moduleID);
+        return knex('quiz').select("*").where('module_id', '=', moduleID);
     }
 
     static getQuiz(courseID, moduleID, quizID) {
-        return knex('modules').select("*").where('course_id', '=', courseID).andWhere('module_id', '=', moduleID).andWhere('quiz_id', '=', quizID);
+        return knex("quiz")
+               .select('quiz.*')
+               .innerJoin('module', 'quiz.MODULE_ID', '=', 'module.MODULE_ID')
+               .innerJoin('course', 'module.COURSE_ID', '=', 'course.COURSE_ID')
+               .where({
+                   "quiz.QUIZ_ID": quizID,
+                   "module.MODULE_ID": moduleID,
+                   "course.COURSE_ID": courseID
+               });
+    }
+    
+    static createQuiz(data) {
+        return knex('quiz').insert({
+            QUIZ_NAME: data["QUIZ_NAME"],
+            MODULE_ID: data["MODULE_ID"],
+            QUESTION_ORDER: JSON.stringify(data["QUESTION_ORDER"]),
+            QUIZ_MAXTRIES: data["QUIZ_MAXTRIES"]
+        });
+    }
+    
+
+    static updateQuiz(moduleID, quizID, data) {
+        return knex('quiz')
+               .update(data)
+               .where("QUIZ_ID", "=", quizID)
+               .andWhere("MODULE_ID", "=", moduleID);
     }
 
-    static createQuiz(courseID, moduleID, quizData) {
-        return knex('modules').where('course_id', '=', courseID).andWhere('module_id', '=', moduleID).insert(quizData);
-    }
-
-    static updateQuiz(courseID, moduleID, quizID, quizData) {
-        return knex('modules').where('course_id', '=', courseID).andWhere('module_id', '=', moduleID).andWhere('quiz_id', '=', quizID).update(quizData);
-    }
-
-    static deleteQuiz(courseID, moduleID, quizID) {
-        return knex('modules').where('course_id', '=', courseID).andWhere('module_id', '=', moduleID).andWhere('quiz_id', '=', quizID).del();
+    static deleteQuiz(quizID) {
+        return knex('quiz').where("QUIZ_ID", "=", quizID).del();
     }
 }
 
