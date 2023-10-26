209f8767d7c35830e18d74d159964078f438a395
Kriss Andrew <Kriss1Andrew@gmail.com>
2023-10-26 18:14:17 +1000

    Added lecture creation functionality

--- ui/app/courses/[courseId]/[moduleId]/page.jsx
+++ ui/app/courses/[courseId]/[moduleId]/page.jsx
@@ -8,6 +8,15 @@ import QuizOverview from "@/components/quizOverview"; // Import the new ModuleOv
 import { AuthContext } from "@/app/auth.jsx";
 import { Box, Grid } from "@mui/material";
 
+// imports for create pop up
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import TextField from "@mui/material/TextField";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+
+
 export default function ModulePage({ params }) {
   const [lectures, setLectures] = useState(null);
   const [quizzes, setQuizzes] = useState(null);
@@ -15,57 +24,91 @@ export default function ModulePage({ params }) {
   const { authToken } = useContext(AuthContext);
   const { userType } = useContext(AuthContext);
 
+  // constants for create pop up
+  const [open, setOpen] = useState(false);
+  const [lectureName, setLectureName] = useState("");
+  const [lectureOrder, setLectureOrder] = useState("");
+
+  const handleCreateLecture = async () => {
+    try {
+      const response = await fetch(`https://jcmg-api.herokuapp.com/api/course/module/lecture/create?moduleID=${params.moduleId}`, {
+        method: "POST",
+        headers: {
+          "accept": "application/json",
+          "Authorization": `Bearer ${authToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          lecture_name: lectureName,
+          lecture_order: Number(lectureOrder),
+        }),
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        fetchLectures();
+      } else {
+        console.error("Failed to create lecture. Status:", response.status);
+      }
+    } catch (error) {
+      console.error("Error creating lecture:", error);
+    }
+    // close dialog after attempting lecture creation
+    setOpen(false);
+  };
+
+  async function fetchLectures() {
+    try {
+      const url = `https://jcmg-api.herokuapp.com/api/course/module/lectures?courseID=${params.courseId}&moduleID=${params.moduleId}`;
+      console.log("Fetching from URL:", url); // Log the URL to check if course and module IDs are correct
+
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        console.error("Error response from server:", response);
+        return; // If response is not ok, log the error and return
+      }
+
+      const data = await response.json();
+      console.log("Data: ", data);
+      setLectures(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching course data:", error);
+    }
+  }
+
+  async function fetchQuizzes() {
+    try {
+      const url = `https://jcmg-api.herokuapp.com/api/course/module/quizzes?moduleID=${params.moduleId}`;
+      console.log("Fetching from URL:", url); // Log the URL to check if course and module IDs are correct
+
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        console.error("Error response from server:", response);
+        return; // If response is not ok, log the error and return
+      }
+
+      const data = await response.json();
+      console.log("Data: ", data);
+      setQuizzes(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching course data:", error);
+    }
+  }
+
   useEffect(() => {
-    async function fetchLectures() {
-      try {
-        const url = `https://jcmg-api.herokuapp.com/api/course/module/lectures?courseID=${params.courseId}&moduleID=${params.moduleId}`;
-        console.log("Fetching from URL:", url); // Log the URL to check if course and module IDs are correct
-
-        const response = await fetch(url, {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-        });
-
-        if (!response.ok) {
-          console.error("Error response from server:", response);
-          return; // If response is not ok, log the error and return
-        }
-
-        const data = await response.json();
-        console.log("Data: ", data);
-        setLectures(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error fetching course data:", error);
-      }
-    }
-
-    async function fetchQuizzes() {
-      try {
-        const url = `https://jcmg-api.herokuapp.com/api/course/module/quizzes?moduleID=${params.moduleId}`;
-        console.log("Fetching from URL:", url); // Log the URL to check if course and module IDs are correct
-
-        const response = await fetch(url, {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-        });
-
-        if (!response.ok) {
-          console.error("Error response from server:", response);
-          return; // If response is not ok, log the error and return
-        }
-
-        const data = await response.json();
-        console.log("Data: ", data);
-        setQuizzes(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error fetching course data:", error);
-      }
-    }
-
     fetchLectures();
     fetchQuizzes();
   }, [params.courseId, params.moduleId, authToken]);
@@ -91,7 +134,42 @@ export default function ModulePage({ params }) {
     <main className={styles.main}>
       <div className={styles.contentWrapper}>
         <div className={styles.description}>
-          <h1 className={styles.title}>{lectures && lectures.COURSE_NAME}</h1>
+        <h1 className={styles.title}>{lectures && lectures.COURSE_NAME}</h1>
+          {userType === "admin" && (
+            <>
+              <Button variant="contained" color="primary" onClick={() => setOpen(true)}>
+                Create Lecture
+              </Button>
+              <Dialog open={open} onClose={() => setOpen(false)}>
+                <DialogTitle>Create a New Lecture</DialogTitle>
+                <DialogContent>
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Lecture Name"
+                    type="text"
+                    fullWidth
+                    value={lectureName}
+                    onChange={(e) => setLectureName(e.target.value)}
+                  />
+                  <TextField
+                    margin="dense"
+                    label="Lecture Order"
+                    type="number"
+                    fullWidth
+                    value={lectureOrder}
+                    onChange={(e) => setLectureOrder(e.target.value)}
+                  />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={() => setOpen(false)}>Cancel</Button>
+                  <Button onClick={handleCreateLecture} color="primary">
+                    Create
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </>
+          )}
           <Grid container>
             <Grid xs={12} md={6}>
               {/* Updated Section for Lectures */}
